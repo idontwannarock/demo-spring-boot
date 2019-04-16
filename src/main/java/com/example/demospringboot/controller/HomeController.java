971d0dc2ec585bf24e849696e969cb702159faee
HowardWang <Howard.Wang@want-want.com>
2019-04-16 16:22:10 +0800

    1. Spring Security 的 RESTful 基礎設定 2. 修改 README.md

--- src/main/java/com/example/demospringboot/controller/HomeController.java
+++ src/main/java/com/example/demospringboot/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demospringboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("home")
@@ -25,5 +26,10 @@ public class HomeController {
     @GetMapping("/admin")
     public ResponseEntity<String> helloAdmin() {
         return ResponseEntity.ok("This is the admin area of home running at port: " + environment.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/username")
+    public ResponseEntity<String> helloUser(@AuthenticationPrincipal String username) {
+        return ResponseEntity.ok("Hello " + username + "!");
     }
 }
---
+++ src/main/java/com/example/demospringboot/security/CustomWebSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.demospringboot.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class CustomWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .antMatchers(HttpMethod.GET, "/home/username").permitAll()
+                .and()
+                .httpBasic();
+    }
+
+    // Spring has UserDetailsService interface, which can be overridden to provide
+    // our implementation for fetching user from database (or any other source).
+    // Here we use in-memory authentication that comes with Spring Security itself.
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(encoder().encode("adminPass")).roles("ADMIN")
+                .and()
+                .withUser("user").password(encoder().encode("userPass")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
