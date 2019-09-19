7ffd8cb4ad3d80f2119a01a60534421838d7c44c
Howard Wang <idontwannarock@gmail.com>
2019-09-19 15:26:52 +0800

    改用 WebMvcRegistrations 設定 API 版本控制

--- src/main/java/com/example/demospringboot/configuration/CustomWebMvcConfigurer.java
+++ src/main/java/com/example/demospringboot/configuration/CustomWebMvcConfigurer.java
@@ -1,8 +1,11 @@
 package com.example.demospringboot.configuration;
 
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
 public class CustomWebMvcConfigurer implements WebMvcConfigurer {
@@ -13,5 +16,15 @@ public class CustomWebMvcConfigurer implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public WebMvcRegistrations webMvcRegistrationsRequestMappingHandlerMapping() {
+        return new WebMvcRegistrations() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new ApiVersionHandlerMapping();
+            }
+        };
     }
 }
